42b1b9db59df26dea88183f2a70baacc77d9708a
AhmadouAidara <ahmadou.aidara@enpc.fr>
2024-09-05 16:27:04 +0200

    ask_question_to_pdf.py

--- hackathon-ponts/src/utils/ask_question_to_pdf.py
+++ hackathon-ponts/src/utils/ask_question_to_pdf.py
@@ -6,39 +6,7 @@ from dotenv import load_dotenv
 from nltk.tokenize import sent_tokenize
 from openai import OpenAI
 
-
-
-
-def open_file(filepath):
-    with open(filepath, "r", encoding="utf-8") as infile:
-        return infile.read()
-
-
-openai.api_key = os.getenv("OPENAI_API_KEY")
-openai.organization = os.getenv("OPENAI_ORGANIZATION")
-
-
-def read_pdf(filename):
-    context = ""
-
-    # Open the PDF file
-    with fitz.open(filename) as pdf_file:
-        # Get the number of pages in the PDF file
-        num_pages = pdf_file.page_count
-
-        # Loop through each page in the PDF file
-        for page_num in range(num_pages):
-            # Get the current page
-            page = pdf_file[page_num]
-
-            # Get the text from the current page
-            page_text = page.get_text().replace("\n", "")
-
-            # Append the text to context
-            context += page_text
-    return context
-
-
+client = OpenAI()
 def split_text(text, chunk_size=5000):
     chunks = []
     current_chunk = StringIO()
@@ -66,13 +34,33 @@ def split_text(text, chunk_size=5000):
         chunks.append(current_chunk.getvalue())
     return chunks
 
+def read_pdf(filename):
+    context = ""
+
+    # Open the PDF file
+    with fitz.open(filename) as pdf_file:
+        # Get the number of pages in the PDF file
+        num_pages = pdf_file.page_count
+
+        # Loop through each page in the PDF file
+        for page_num in range(num_pages):
+            # Get the current page
+            page = pdf_file[page_num]
+
+            # Get the text from the current page
+            page_text = page.get_text().replace("\n", "")
+
+            # Append the text to context
+            context += page_text
+    return context
 
 filename = os.path.join(os.path.dirname(__file__), "filename.pdf")
 document = read_pdf(filename)
 chunks = split_text(document)
 
-text = chunks[0]
-
+text=chunks[0]
+qst="Résume ce texte"
+list=[]
 
 def gpt3_completion(question):
     list.append({"role":"user","content":question})
@@ -84,14 +72,27 @@ def gpt3_completion(question):
 load_dotenv()
 
 
-# Exemple d'utilisation
-question = "Résume ce texte"
-response = gpt3_completion(question)
-print(response)
+def open_file(filepath):
+    with open(filepath, "r", encoding="utf-8") as infile:
+        return infile.read()
 
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.organization = os.getenv("OPENAI_ORGANIZATION")
+
+
+
+
+
+
+
+
+# gpt3_completion(qst)
 
 def ask_question_to_pdf(question):
     return gpt3_completion(question)
 
 
-ask_question_to_pdf(question)
+
+
+print(ask_question_to_pdf(qst))
